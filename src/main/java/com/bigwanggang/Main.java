f9a22516e47867ce9a5be389d244869af1be29ca
bigwanggang <bigwanggang@163.com>
2017-03-18 10:19:01 +0800

    first commit

---
+++ src/main/java/com/bigwanggang/Main.java
@@ -0,0 +1,10 @@
+package com.bigwanggang;
+
+/**
+ * Created by gustaov on 2017/3/18.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("helloworld");
+    }
+}
