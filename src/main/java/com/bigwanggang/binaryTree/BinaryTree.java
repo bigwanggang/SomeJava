4a5102e546d74c59eaad71971e425db9c2369030
bigwanggang <bigwanggang@163.com>
2017-07-19 19:04:56 +0800

    Create BinaryTree.java

---
+++ src/main/java/com/bigwanggang/binaryTree/BinaryTree.java
@@ -0,0 +1,11 @@
+package com.bigwanggang.binaryTree;
+
+public class BinaryTree {
+    public BinaryTree(Object element) {
+        this.element = element;
+    }
+
+    Object element;
+    BinaryTree left;
+    BinaryTree right;
+}
