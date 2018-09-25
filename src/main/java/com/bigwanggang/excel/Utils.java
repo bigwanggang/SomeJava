b80aaf9e7472cc83908978a110fe2b02a4ac0294
bigwanggang <bigwanggang@163.com>
2018-09-25 22:47:23 +0800

    add excel test

---
+++ src/main/java/com/bigwanggang/excel/Utils.java
@@ -0,0 +1,25 @@
+package com.bigwanggang.excel;
+
+
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+public class Utils {
+    private static final String EXCEL_XLS = ".xls";
+    private static final String EXCEL_XLSX = ".xlsx";
+    public static Workbook getWorkbok(File file) throws IOException {
+        Workbook wb = null;
+        FileInputStream in = new FileInputStream(file);
+        if (file.getName().endsWith(EXCEL_XLS)) {     //Excel&nbsp;2003
+            wb = new HSSFWorkbook(in);
+        } else if (file.getName().endsWith(EXCEL_XLSX)) {    // Excel 2007/2010
+            wb = new XSSFWorkbook(in);
+        }
+        return wb;
+    }
+}
