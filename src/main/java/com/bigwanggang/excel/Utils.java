adbcb4813370c69c860f067c13b5d0eb9f904b61
bigwanggang <bigwanggang@163.com>
2018-09-26 09:26:23 +0800

    Update Utils.java

--- src/main/java/com/bigwanggang/excel/Utils.java
+++ src/main/java/com/bigwanggang/excel/Utils.java
@@ -8,11 +8,16 @@ import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 public class Utils {
     private static final String EXCEL_XLS = ".xls";
     private static final String EXCEL_XLSX = ".xlsx";
-    public static Workbook getWorkbok(File file) throws IOException {
+
+    public static Workbook getWorkbook(File file) throws IOException {
         Workbook wb = null;
         FileInputStream in = new FileInputStream(file);
         if (file.getName().endsWith(EXCEL_XLS)) {     //Excel&nbsp;2003
@@ -22,4 +27,50 @@ public class Utils {
         }
         return wb;
     }
+
+    public static Connection getConn() {
+        String driver = "com.mysql.jdbc.Driver";
+        String url = "jdbc:mysql://localhost:3306/test1";
+        String username = "root";
+        String password = "root123";
+        Connection conn = null;
+        try {
+            Class.forName(driver); //classLoader,加载对应驱动
+            conn = (Connection) DriverManager.getConnection(url, username, password);
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return conn;
+    }
+
+    public static int insert(Info info) {
+        Connection conn = getConn();
+        int i = 0;
+        String sql = "insert into info (Name,age,height, phone, sex,location, job," +
+                "hometown, education, major, message) values(?,?,?,?,?,?,?,?,?,?,?)";
+        PreparedStatement pstmt;
+        try {
+            pstmt = (PreparedStatement) conn.prepareStatement(sql);
+            pstmt.setString(1, info.getName());
+            pstmt.setString(2, info.getAge());
+            pstmt.setString(3, info.getHeight());
+            pstmt.setString(4, info.getPhone());
+            pstmt.setString(5, info.getSex());
+            pstmt.setString(6, info.getLocation());
+            pstmt.setString(7, info.getJob());
+            pstmt.setString(8, info.getHometown());
+            pstmt.setString(9, info.getEducation());
+            pstmt.setString(10, info.getMajor());
+            pstmt.setString(11, info.getMessage());
+            i = pstmt.executeUpdate();
+            pstmt.close();
+            conn.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return i;
+    }
+
 }
