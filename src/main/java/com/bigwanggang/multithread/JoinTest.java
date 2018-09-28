def0cf92b2635530cbc4e5e41ee7882cdbbe7867
bigwanggang <bigwanggang@163.com>
2018-09-28 15:28:45 +0800

    Create JoinTest.java

---
+++ src/main/java/com/bigwanggang/multithread/JoinTest.java
@@ -0,0 +1,21 @@
+package com.bigwanggang.multithread;
+
+public class JoinTest {
+    public static void main(String[] args) throws InterruptedException {
+        for (int i = 0; i < 10; i++) {
+            Thread t = new Thread(new MyRunnable());
+            t.start();
+//            t.join();
+        }
+        System.out.println("main");
+    }
+    static class MyRunnable implements Runnable{
+
+        @Override
+        public void run() {
+            for (int i = 0; i < 10; i++) {
+                System.out.println(Thread.currentThread().getName() + ": " + i);
+            }
+        }
+    }
+}
