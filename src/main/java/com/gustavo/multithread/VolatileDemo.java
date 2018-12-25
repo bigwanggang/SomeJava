bb25adc06008248a457db9432dba2697aece6da8
gustavoNull <bigwanggang@163.com>
2018-12-25 12:40:42 +0800

    Create VolatileDemo.java

---
+++ src/main/java/com/gustavo/multithread/VolatileDemo.java
@@ -0,0 +1,27 @@
+package com.gustavo.multithread;
+
+import java.util.concurrent.TimeUnit;
+
+public class VolatileDemo {
+    private int a = 0 ;
+    private  volatile boolean flag=true;
+    public static void main(String[] args) {
+        VolatileDemo instance = new VolatileDemo();
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while (instance.flag) {
+                    instance.a ++;
+                }
+                System.out.println(instance.a);
+            }
+        }).start();
+        try {
+            TimeUnit.SECONDS.sleep(2);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        instance.flag = false;
+        System.out.println("set false");
+    }
+}
