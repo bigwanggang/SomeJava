4bdee8c7c935427bbaf3739c7aadd6af5d9c0b7e
gustavoNull <bigwanggang@163.com>
2019-01-18 17:25:18 +0800

    Create UtilsTest.java

---
+++ src/test/java/com/gustavo/sort/heapSort/UtilsTest.java
@@ -0,0 +1,32 @@
+package com.gustavo.sort.heapSort;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class UtilsTest {
+
+    @Test
+    public void left() {
+        assertEquals(5, Utils.left(2));
+        assertEquals(1, Utils.left(0));
+        assertEquals(3, Utils.left(1));
+    }
+
+    @Test
+    public void right() {
+        assertEquals(6, Utils.right(2));
+        assertEquals(2, Utils.right(0));
+        assertEquals(4, Utils.right(1));
+    }
+
+    @Test
+    public void testParent(){
+        assertEquals(0, Utils.parent(1));
+        assertEquals(0, Utils.parent(2));
+        assertEquals(1, Utils.parent(3));
+        assertEquals(1, Utils.parent(4));
+        assertEquals(2, Utils.parent(5));
+        assertEquals(2, Utils.parent(6));
+    }
+}
