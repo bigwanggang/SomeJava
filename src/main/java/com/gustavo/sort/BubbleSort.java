4227e64d328225e276da478dbb7ff0976bd87a5e
gustavoNull <bigwanggang@163.com>
2019-01-18 08:21:34 +0800

    Create BubbleSort.java

---
+++ src/main/java/com/gustavo/sort/BubbleSort.java
@@ -0,0 +1,40 @@
+package com.gustavo.sort;
+
+import java.util.Random;
+
+public class BubbleSort {
+    public static final int num = 5;
+    public static int[] array = {5,4,3,2,1};
+
+    public static void main(String[] args) {
+//        initArray();
+        printArray();
+        for (int i = 0; i < num -1; i++) {
+
+            for (int j = 0; j < num-  i - 1; j++) {
+                if (array[j + 1] < array[j]) {
+                    int tmp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = tmp;
+                }
+                printArray();
+            }
+            System.out.println("--");
+        }
+    }
+
+    private static void printArray() {
+        for (int i = 0; i < num; i++) {
+            System.out.print(array[i] + "\t");
+        }
+        System.out.println();
+    }
+
+    public static void initArray() {
+        Random random = new Random();
+        for (int i = 0; i < num; i++) {
+            array[i] = random.nextInt(100);
+        }
+    }
+
+}
