84e396a8e296a527b72ddc8429c7336720818baa
gustavoNull <bigwanggang@163.com>
2019-01-21 10:00:56 +0800

    Update Utils.java

--- src/main/java/com/gustavo/sort/heapSort/Utils.java
+++ src/main/java/com/gustavo/sort/heapSort/Utils.java
@@ -1,21 +1,23 @@
 package com.gustavo.sort.heapSort;
 
+import java.util.Random;
+
 /**
  * CLR
  */
 public class Utils {
-    public static void maxHeapify(int[] array, int i) {
+
+    public static void maxHeapify(int[] array, int heapSize, int i) {
         int largest = i;
-        int len = array.length;
         int l = left(i);
         int r = right(i);
-        if (l < len && array[l] > array[i])
+        if (l < heapSize && array[l] > array[i])
             largest = l;
-        if (r < len && array[r] > array[largest])
+        if (r < heapSize && array[r] > array[largest])
             largest = r;
         if (largest != i) {
             swap(array, i, largest);
-            maxHeapify(array, largest);
+            maxHeapify(array, heapSize, largest);
         }
     }
 
@@ -31,7 +33,6 @@ public class Utils {
         return (i + 1) / 2 - 1;
     }
 
-
     public static void swap(int[] array, int i, int j) {
         int tmp = array[i];
         array[i] = array[j];
@@ -44,5 +45,29 @@ public class Utils {
             System.out.print(array[i] + "\t");
         }
         System.out.println();
+    }
+
+    public static void heapSort(int[] array) {
+        buildMaxHeap(array);
+        for (int i = array.length - 1; i > 0; i--) {
+            swap(array, 0, i);
+            maxHeapify(array, i, 0);
+        }
+    }
+
+    public static void buildMaxHeap(int[] array) {
+        int heapSize = array.length;
+        for (int i = array.length / 2; i >= 0; i--) {
+            maxHeapify(array, heapSize, i);
+        }
+    }
+
+    public static int[] createArray(int num, int limit) {
+        Random random = new Random();
+        int[] array = new int[num];
+        for (int i = 0; i < num; i++) {
+            array[i] = random.nextInt(limit);
+        }
+        return array;
     }
 }
