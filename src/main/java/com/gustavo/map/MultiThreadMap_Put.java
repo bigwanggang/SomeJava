bede056a49904be39f59317552481ba2ea02afa3
gustavoNull <bigwanggang@163.com>
2019-02-01 11:24:30 +0800

    Create MultiThreadMap_Put.java

---
+++ src/main/java/com/gustavo/map/MultiThreadMap_Put.java
@@ -0,0 +1,28 @@
+package com.gustavo.map;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+
+public class MultiThreadMap_Put {
+    static Map<String, String> map = new HashMap<>();
+
+    public static void main(String[] args) throws InterruptedException {
+        for (int i = 0; i < 100; i++) {
+            new Thread(new Putting(), "thread###" + i).start();
+        }
+
+        TimeUnit.SECONDS.sleep(5);
+        System.out.println(map.size());
+    }
+
+    static class Putting implements Runnable {
+
+        @Override
+        public void run() {
+            for (int i = 0; i < 100000; i++) {
+                map.put(Thread.currentThread().getName() + "###" + i, i+"");
+            }
+        }
+    }
+}
