b2bbe66eef0e067fbce3373a67c91ba846322a95
OhLaughing <bigwanggang@163.com>
2019-04-24 15:27:29 +0800

    Create RemoveTask.java

---
+++ src/main/java/com/gustavo/blockingqueue/delayqueue/demo/RemoveTask.java
@@ -0,0 +1,26 @@
+package com.gustavo.blockingqueue.delayqueue.demo;
+
+import java.util.Iterator;
+import java.util.concurrent.DelayQueue;
+
+public class RemoveTask implements Runnable {
+    private String removeString;
+    private DelayQueue<Message> queue;
+
+    public RemoveTask(String s, DelayQueue<Message> queue) {
+        this.removeString = s;
+        this.queue = queue;
+    }
+
+    @Override
+    public void run() {
+        Iterator<Message> iterator = queue.iterator();
+        while (iterator.hasNext()) {
+            Message m = iterator.next();
+            if (removeString.equals(m.getMsg())) {
+                iterator.remove();
+                System.out.println("remove: " + removeString + " at " + System.currentTimeMillis());
+            }
+        }
+    }
+}
