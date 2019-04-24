cf4f4f2fdeada34d71c50d7e9fca2d823222501d
OhLaughing <bigwanggang@163.com>
2019-04-24 14:36:50 +0800

    Update Message.java

--- src/main/java/com/gustavo/blockingqueue/delayqueue/demo/Message.java
+++ src/main/java/com/gustavo/blockingqueue/delayqueue/demo/Message.java
@@ -1,25 +1,39 @@
-package com.gustavo.blockingqueue.delayqueue.demo;
+package com.gustavo.delayqueue;
 
 import java.util.concurrent.Delayed;
 import java.util.concurrent.TimeUnit;
 
-/**
- * Created by gustaov on 2019/4/18.
- */
 public class Message implements Delayed {
     private long executeTime;
+    private String msg;
 
-    public Message(long milliseconds) {
-        this.executeTime = TimeUnit.NANOSECONDS.convert(milliseconds, TimeUnit.MILLISECONDS) + System.nanoTime();
+    public Message(String msg, long executeTime) {
+        this.msg = msg;
+        this.executeTime = TimeUnit.NANOSECONDS.convert(executeTime, TimeUnit.SECONDS) + System.nanoTime();
     }
 
     @Override
     public long getDelay(TimeUnit unit) {
-        return this.executeTime - System.nanoTime();
+        return executeTime - System.nanoTime();
     }
 
     @Override
     public int compareTo(Delayed o) {
-        return 0;
+        Message other = (Message) o;
+        long diff = executeTime - other.executeTime;
+        if (diff > 0) {
+            return 1;
+        } else if (diff < 0) {
+            return -1;
+        } else {
+            return 0;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Message{" +
+                "msg='" + msg + '\'' +
+                '}';
     }
 }
