fc8a9369ff8a122312a942e0afda150601680ab9
OhLaughing <bigwanggang@163.com>
2019-05-08 17:27:03 +0800

    Create SmallestInRotatedSortedArrayTest.java

---
+++ src/test/java/com/gustavo/dataStructureAndAlgo/SmallestInRotatedSortedArrayTest.java
@@ -0,0 +1,17 @@
+package com.gustavo.dataStructureAndAlgo;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class SmallestInRotatedSortedArrayTest {
+
+    @Test
+    public void searchSmallest() {
+        int[] nums = {1};
+        Assert.assertEquals(1, SmallestInRotatedSortedArray.searchSmallest(nums));
+        int[] nums1 = {5, 6, 7, 1, 2, 3, 4};
+        Assert.assertEquals(1, SmallestInRotatedSortedArray.searchSmallest(nums1));
+        int[] nums2 = {5, 6, 7, 1,  3, 4};
+        Assert.assertEquals(1, SmallestInRotatedSortedArray.searchSmallest(nums2));
+    }
+}
