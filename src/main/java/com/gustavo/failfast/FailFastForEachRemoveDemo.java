cebd10cdcf46038be1411f0a5f24918a57abcf8e
OhLaughing <bigwanggang@163.com>
2019-05-13 09:32:35 +0800

    Create FailFastForEachRemoveDemo.java

---
+++ src/main/java/com/gustavo/failfast/FailFastForEachRemoveDemo.java
@@ -0,0 +1,19 @@
+package com.gustavo.failfast;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FailFastForEachRemoveDemo {
+    public static void main(String[] args) {
+        List<String> list = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            list.add(Thread.currentThread().getName() + "-" + i);
+        }
+        for (String s : list) {
+            if ("main-5".equals(s))
+                list.remove(s);
+        }
+
+        System.out.println(list);
+    }
+}
