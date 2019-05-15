2cd8c7eb9611f5402640f3ad85df2c37734a6041
OhLaughing <bigwanggang@163.com>
2019-05-15 15:26:35 +0800

    Update ThreadStatusDemo_Join.java

--- src/main/java/com/gustavo/thread/ThreadStatusDemo_Join.java
+++ src/main/java/com/gustavo/thread/ThreadStatusDemo_Join.java
@@ -2,21 +2,26 @@ package com.gustavo.thread;
 
 import java.util.concurrent.TimeUnit;
 
+/**
+ * Thread.join的栗子: MyRunning线程sleep 3 秒
+ * Joinning线程对MyRunning线程执行join，因此oinning线程会在MyRunning线程执行完之后才执行
+ * 在等待MyRunning线程的时间段中，Joinning的状态是：WAITING
+ */
 public class ThreadStatusDemo_Join {
     public static void main(String[] args) throws InterruptedException {
-        Thread t1 = new Thread(new Joinning());
-        Thread t2 = new Thread(new Joinning1(t1));
+        Thread t1 = new Thread(new MyRunning());
+        Thread t2 = new Thread(new Joinning(t1));
         t1.start();
         t2.start();
         TimeUnit.SECONDS.sleep(1);
-        System.out.println(t2.getState());
+        System.out.println("State of Joinning: " + t2.getState());
     }
 
-    static class Joinning1 implements Runnable {
+    static class Joinning implements Runnable {
 
         Thread thread;
 
-        public Joinning1(Thread thread) {
+        public Joinning(Thread thread) {
             this.thread = thread;
         }
 
@@ -30,7 +35,7 @@ public class ThreadStatusDemo_Join {
         }
     }
 
-    static class Joinning implements Runnable {
+    static class MyRunning implements Runnable {
         @Override
         public void run() {
             System.out.println("in thread.");
