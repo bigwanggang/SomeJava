f02a4e7364d9c040aaf1a3b12240f12a8992da1e
OhLaughing <bigwanggang@163.com>
2019-05-21 18:43:14 +0800

    Create ListNode.java

---
+++ src/main/java/com/gustavo/dataStructureAndAlgo/pointOffer/ListNode.java
@@ -0,0 +1,10 @@
+package com.gustavo.dataStructureAndAlgo.pointOffer;
+
+public class ListNode {
+    int val;
+    ListNode next = null;
+
+    ListNode(int val) {
+        this.val = val;
+    }
+}
