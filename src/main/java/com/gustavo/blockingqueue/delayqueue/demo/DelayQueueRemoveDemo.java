7d03a4274d9decb15b7bc3fbb5392823924a627a
OhLaughing <bigwanggang@163.com>
2019-05-24 15:36:38 +0800

    Create DelayQueueRemoveDemo.java

---
+++ src/main/java/com/gustavo/blockingqueue/delayqueue/demo/DelayQueueRemoveDemo.java
@@ -0,0 +1,31 @@
+package com.gustavo.blockingqueue.delayqueue.demo;
+
+import java.util.concurrent.DelayQueue;
+
+/**
+ * 验证个问题：看代码发现在delayQueue中take会阻塞, 先放进DelayQueue里一个长时间的任务
+ * 任何启动线程take，之后再往DelayQueue里加一个时间比较短的任务
+ */
+public class DelayQueueRemoveDemo {
+    public static void main(String[] args) {
+        DelayQueue<Message> delayQueue = new DelayQueue<>();
+        boolean b = delayQueue.offer(new Message("hello", 10));
+        System.out.println(b);
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while (true) {
+                    try {
+                        Message m = delayQueue.take();
+                        System.out.println(m);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }).start();
+
+        boolean b1 = delayQueue.offer(new Message("world", 1));
+        System.out.println(b1);
+    }
+}
