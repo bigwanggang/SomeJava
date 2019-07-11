853a9b5995f96ce889b8c8b1610e736a1cdab99d
OhLaughing <bigwanggang@163.com>
2019-07-11 16:31:27 +0800

    Create Student.java

---
+++ src/main/java/com/gustavo/copy/Student.java
@@ -0,0 +1,50 @@
+package com.gustavo.copy;
+
+public class Student implements Cloneable{
+    private String id;
+    private String name;
+    private Address address;
+
+    public Student(String id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
+    @Override
+    public Object clone() throws CloneNotSupportedException {
+        return super.clone();
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                ", address=" + address +
+                '}';
+    }
+}
