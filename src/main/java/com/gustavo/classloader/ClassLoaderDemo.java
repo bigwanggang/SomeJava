4680d189a419e052e2fd0d920fbe7e1cb8d9d6c9
OhLaughing <bigwanggang@163.com>
2019-07-25 17:09:22 +0800

    Update ClassLoaderDemo.java

--- src/main/java/com/gustavo/classloader/ClassLoaderDemo.java
+++ src/main/java/com/gustavo/classloader/ClassLoaderDemo.java
@@ -1,5 +1,7 @@
 package com.gustavo.classloader;
 
+import sun.util.resources.CalendarData_ar;
+
 /**
  * Created by gustaov on 2019/6/16.
  */
@@ -7,6 +9,17 @@ public class ClassLoaderDemo {
     public static void main(String[] args) {
         System.out.println(ClassLoader.getSystemClassLoader());
         ClassLoader c = ClassLoaderDemo.class.getClassLoader();
+        // String 的classloader 为bootstrapClassLoader，在java中为null
+        String s = "a";
+        System.out.println(s.getClass().getClassLoader());
         System.out.println(c);
+        System.out.println(System.getProperty("sun.boot.class.path"));
+        System.out.println(System.getProperty("java.class.path"));
+        System.out.println(System.getProperty("java.ext.dirs"));
+
+        //CalendarData_ar的classLoaderw为extClassLoader
+        Class clazz = CalendarData_ar.class;
+        System.out.println(clazz.getClassLoader());
+
     }
 }
