f8e9a153190ab9fd6bbaf0a3efd1c6156ac096c8
OhLaughing <bigwanggang@163.com>
2019-08-13 19:14:04 +0800

    Create IdGenerator.java

---
+++ src/main/java/com/gustavo/distribute/MultiProcessIdGeneratorFailure/IdGenerator.java
@@ -0,0 +1,25 @@
+package com.gustavo.distribute.MultiProcessIdGeneratorFailure;
+
+import java.util.concurrent.locks.Lock;
+
+/**
+ * Created by gustaov on 2019/8/12.
+ */
+public class IdGenerator {
+    private static int i = 1;
+    private Lock lock;
+
+    public IdGenerator(Lock lock) {
+        this.lock = lock;
+    }
+
+    public int nextId() {
+        lock.lock();
+        try {
+            return i++;
+        } finally {
+            lock.unlock();
+        }
+    }
+
+}
