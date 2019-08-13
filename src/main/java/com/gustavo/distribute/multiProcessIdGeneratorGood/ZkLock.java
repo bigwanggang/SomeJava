0323c7b7633fe07ab4ea72ca47f548c467484d57
OhLaughing <bigwanggang@163.com>
2019-08-13 19:42:01 +0800

    Update ZkLock.java

--- src/main/java/com/gustavo/distribute/multiProcessIdGeneratorGood/ZkLock.java
+++ src/main/java/com/gustavo/distribute/multiProcessIdGeneratorGood/ZkLock.java
@@ -1,5 +1,6 @@
 package com.gustavo.distribute.multiProcessIdGeneratorGood;
 
+import lombok.extern.slf4j.Slf4j;
 import org.I0Itec.zkclient.IZkDataListener;
 import org.I0Itec.zkclient.ZkClient;
 import org.I0Itec.zkclient.exception.ZkNodeExistsException;
@@ -9,6 +10,7 @@ import java.util.concurrent.TimeUnit;
 import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.Lock;
 
+@Slf4j
 public class ZkLock implements Lock {
     private ZkClient client;
     private String lockPath;
@@ -30,20 +32,20 @@ public class ZkLock implements Lock {
 
                 @Override
                 public void handleDataDeleted(String s) throws Exception {
-                    System.out.println("receive delete info: " + s);
+                    log.info(Thread.currentThread().getName() + "### receive delete info: " + s);
                     latch.countDown();
                 }
             };
             client.subscribeDataChanges(lockPath, listener);
             try {
-                latch.await();
+                latch.await(500, TimeUnit.MILLISECONDS);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
             client.unsubscribeDataChanges(lockPath, listener);
             lock();
         }
-        System.out.println(Thread.currentThread().getName() + "get lock");
+        log.info(Thread.currentThread().getName() + "get lock");
     }
 
     @Override
@@ -54,7 +56,7 @@ public class ZkLock implements Lock {
     public boolean tryLock() {
         try {
             client.createEphemeral(lockPath, "aa");
-            System.out.println("try lock success");
+            log.info(Thread.currentThread().getName() + "###try lock success");
             return true;
         } catch (ZkNodeExistsException exception) {
         }
@@ -69,7 +71,8 @@ public class ZkLock implements Lock {
     @Override
     public void unlock() {
         client.delete(lockPath);
-        System.out.println("release lock");
+        log.info(Thread.currentThread().getName() + "###release lock");
+        client.close();
     }
 
     @Override
