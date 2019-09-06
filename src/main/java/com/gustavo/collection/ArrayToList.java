f12243d207fa5a776925648abf2718373a832ef8
OhLaughing <bigwanggang@163.com>
2019-09-06 10:23:10 +0800

    Update ArrayToList.java

--- src/main/java/com/gustavo/collection/ArrayToList.java
+++ src/main/java/com/gustavo/collection/ArrayToList.java
@@ -4,6 +4,9 @@ import java.util.Arrays;
 import java.util.List;
 
 /**
+ * Arrays.asList 是把数组转成List
+ * 但是该list是Arrays的内部ArrayList，并不是常用的那个ArrayList
+ * 该ArrayList没有实现add和remove方法，因此不能执行add和remove
  * Created by gustaov on 2019/1/12.
  */
 public class ArrayToList {
@@ -14,7 +17,7 @@ public class ArrayToList {
         stringArray[2] = "three";
         List<String> stringList = Arrays.asList(stringArray);
         stringList.set(0, "oneList");
-        System.out.println(stringArray[0]);
+        System.out.println(stringArray);
         stringList.add("four");
         stringList.remove(2);
         stringList.clear();
